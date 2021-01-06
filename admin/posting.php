d8afe78f47b8b001bc4404faf16fc114600c1e1e
rikyardi <riky.ardi321@gmail.com>
2021-01-06 15:04:55 +0700

    install bootstrap & CKEDITOR

---
+++ admin/posting.php
@@ -0,0 +1,14 @@
+<h4>Halaman Posting</h4>
+    <form method="POST" action="">
+        <div class="card">
+            <div class="card-body">
+                <label for="judul" class="form-label">Judul</label>
+                <input type="text" class="form-control" id="judul" name="judul">
+                <label for="kategori" class="form-label">Kategori</label>
+                <input type="text" class="form-control" id="kategori" name="kategori"><br>
+                <textarea class="ckeditor" id="myeditor" name="data"></textarea>
+                <br/>
+                <button class="btn btn-primary" type="submit" >Simpan</button>
+                </div>
+        </div>
+    </form>
---
+++ admin/resources.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="../asset/css/style.css">
+<link rel="stylesheet" href="../asset/css/bootstrap.min.css">
+<script src="../asset/js/bootstrap.min.js"></script>
+<script src="../asset/js/jquery-3.5.1.min.js"></script>
+<script src="../asset/js/popper.js"></script>
+<script src="../asset/ckeditor/ckeditor.js"></script>
