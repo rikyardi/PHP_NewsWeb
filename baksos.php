6a3e9491f211662423a27dd3a97e14e3f552b81c
dafimz <dafimuammar31@gmail.com>
2021-01-20 22:03:01 +0700

    progres UI

--- baksos.php
+++ baksos.php
@@ -16,78 +16,17 @@
     }
     </style>
 <body>
-<nav class="navbar navbar-inverse" style="padding: 10px;" >
-    <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#" style="padding: 5px;display:flex">
-            <div style="display: flex;">
-            <img src="asset/img/logo.png" style="width: 50px; height:50px; margin-top:-6px; margin-right:5px">
-            </div>
-            <div style="display: flex;">
-            <p> Mahasiswa & Alumni <br>
-            Universitas Prof. Dr. Hamka</p>
-            </div>
-        </a>
-    </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="index.php">Beranda<span class="sr-only">(current)</span></a></li>
-            <li class="dropdown" data-target="dropdownMahasiswa">
-                <a class="dropdown-toggle" data-toggle="dropdown" target="dropdownMahasiswa" href="#">Mahasiswa & Alumni</a>
-                <ul class="dropdown-menu">
-                    <li>
-                        <a href="scholarship.php"><i class="fa fa-graduation-cap fa-fw"></i> Scholarship</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-trophy fa-fw"></i> Tribute</a>
-                    </li>
-                    <li>
-                        <a href="workinternship.php"><i class="fa fa-building fa-fw"></i> Work & internship</a>
-                    </li>
-                </ul>
-            </li>
-            <li><a href="baksos.php">Membership & Giving</a></li>
-            <li class="dropdown" data-target="dropdownAbout">
-                <a class="dropdown-toggle" data-toggle="dropdown" target="dropdownAbout" href="#">About Us</a>
-                <ul class="dropdown-menu">
-                    <li>
-                        <a href="#"><i class="fa fa-institution fa-fw"></i> Profil</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-history fa-fw"></i> Sejarah</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-        <form class="navbar-form navbar-right">
-            <div class="form-group">
-                <input type="text" class="form-control" onclick="searchLebar()" id="input" placeholder="Search" style="width:100px;">
-            </div>
-            <button type="submit" class="btn btn-default"><i class="fa fa-search fa-fw"></i></button>
-        </form>
-        <!-- <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="#">Link </a></li>
-        </ul> -->
-        </div>
-    </div>
-    </nav>
 
-    <div class="jumbotron" style="text-align: left; margin: 25px 25px; background: lightcyan;">
+<?php include('navbar.php');?>
+
+    <div class="jumbotron" style="text-align: left; margin: 0px auto; margin-top:-20px; background: lightcyan;">
         <h1>Pray For Indonesia in 2021</h1>
         <p>Mahasiswa Peduli kemanusiaan, wadah bagi siapa saja yang ingin membantu saudara-saudara kita yang sedang diterpa bencana alam.</p>
     </div>
 
 
 
-<div class="container-fluid" style= "margin : 50px 50px">
+<div style= "margin : 0px auto; margin-top:-20px">
   <h2></h2>
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
---
+++ navbar.php
@@ -0,0 +1,56 @@
+<nav class="navbar navbar-inverse" style="padding: 10px;" >
+    <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php" style="padding: 5px;display:flex">
+            <div style="display: flex;">
+            <img src="asset/img/logo.png" style="width: 50px; height:50px; margin-top:-6px; margin-right:5px">
+            </div>
+            <div style="display: flex;">
+            <p> Mahasiswa & Alumni <br>
+            Universitas Muhammadiyah Prof. Dr. Hamka</p>
+            </div>
+        </a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.php">Beranda</a></li>
+            <li class="dropdown" data-target="dropdownMahasiswa">
+                <a class="dropdown-toggle" data-toggle="dropdown" target="dropdownMahasiswa" href="#">Mahasiswa & Alumni</a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="scholarship.php"><i class="fa fa-graduation-cap fa-fw"></i> Scholarship</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-trophy fa-fw"></i> Tribute</a>
+                    </li>
+                    <li>
+                        <a href="workinternship.php"><i class="fa fa-building fa-fw"></i> Work & Internship</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li><a href="baksos.php">Membership & Giving</a></li>
+            <li>
+                <a href="about.php">About Us</a>
+            </li>
+        </ul>
+        <form class="navbar-form navbar-right">
+            <div class="form-group">
+                <input type="text" class="form-control" onclick="searchLebar()" id="input" placeholder="Search" style="width:100px;">
+            </div>
+            <button type="submit" class="btn btn-default"><i class="fa fa-search fa-fw"></i></button>
+        </form>
+        <!-- <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#">Link </a></li>
+        </ul> -->
+        </div>
+    </div>
+    </nav>
